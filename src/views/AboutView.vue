250626b569673fbb51d8eb109e0bb1c908b2f9dc
phuongthanhle <phuongthanhle@craniumai.onmicrosoft.com>
2023-10-15 12:21:25 +0700

    alo

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+  <h1> A site for events to better the world.</h1>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
